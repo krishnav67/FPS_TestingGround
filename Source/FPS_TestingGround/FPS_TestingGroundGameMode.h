2720f1e9dfff02c53290b560e42f36079efb852b
krishnav67 <krishnav9719@gmail.com>
2019-10-14 13:09:12 +0530

    Initialize with Git LFS and Added

---
+++ Source/FPS_TestingGround/FPS_TestingGroundGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPS_TestingGroundGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFPS_TestingGroundGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFPS_TestingGroundGameMode();
+};
+
+
+
