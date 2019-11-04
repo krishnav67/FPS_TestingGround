1426346a02915768677dc2e47c5688f2e6bc6665
krishnav67 <krishnav9719@gmail.com>
2019-11-04 16:19:01 +0530

    Deleted few CPP

--- Source/FPS_TestingGround/ChooseNextWaypoint.cpp
+++ Source/FPS_TestingGround/ChooseNextWaypoint.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "Public/ChooseNextWaypoint.h"
+#include "ChooseNextWaypoint.h"
 #include "AIController.h"
-#include "Public/PatrolRoute.h"
+#include "PatrolRoute.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
