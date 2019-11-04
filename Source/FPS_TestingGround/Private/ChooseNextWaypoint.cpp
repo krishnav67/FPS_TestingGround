e4ea6a3467b54e7c583cc85707c3e0e4c19bc550
krishnav67 <krishnav9719@gmail.com>
2019-11-04 15:52:16 +0530

    Composition over Inheritance

--- Source/FPS_TestingGround/Private/ChooseNextWaypoint.cpp
+++ Source/FPS_TestingGround/Private/ChooseNextWaypoint.cpp
@@ -3,18 +3,26 @@
 
 #include "Public/ChooseNextWaypoint.h"
 #include "AIController.h"
-#include "Public/PatrollingGuard.h"
+#include "Public/PatrolRoute.h"
 #include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	// TODO protect against empty portal routes
-
 	// Get Patrol Points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PetrolPointsCPP;
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute))
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	//Warn about empty patrol Routes
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Character is Missing"));
+		return EBTNodeResult::Failed;
+	}
 
 	// Set next Waypoint Points
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
